90187d740a187e917fb5441da5ba43384467075f
swathikrish12393 <swathikrishnaswamy93@gmail.com>
2023-12-04 07:26:50 +0530

    selectionSort

---
+++ selectionSort.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	arr := []int{25, 5, 19, 45, 3, 57}
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		pos := i // Initialize pos with the current index
+
+		// Find the index of the minimum element in the unsorted part of the array
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[pos] {
+				pos = j
+			}
+		}
+
+		// Swap the found minimum element with the first element
+		arr[i], arr[pos] = arr[pos], arr[i]
+	}
+
+	fmt.Println(arr)
+}
